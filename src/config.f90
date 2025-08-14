7394b231cc0748c3f74edae107c71848de377e7e
xumi1993 <gomijianxu@gmail.com>
2025-08-14 08:54:27 -0400

    add cc

---
+++ src/config.f90
@@ -0,0 +1,13 @@
+module constants
+  use, intrinsic :: iso_fortran_env
+  implicit none
+
+  integer, parameter :: cr = real32
+  integer, parameter :: dp = real64
+
+end module constants
+
+module config
+  use constants
+
+end module config
