66d7cec57c96ac524385b78d223930752466e31b
xumi1993 <gomijianxu@gmail.com>
2025-10-02 18:35:08 -0400

    generate good windows

--- src/window_selector/win_sel.f90
+++ src/window_selector/win_sel.f90
@@ -4,10 +4,13 @@ module win_sel
   use cross_correlate
   implicit none
 
+  integer, private, parameter :: max_good_win = 1000
+
   type :: win_config
     real(kind=dp) :: min_cc = 0.4, max_noise = 0.2, jump_fac = 0.1, min_velocity = 2.4, &
-                     threshold_shift_fac = 0.3, threshold_corr = 0.7
-    integer :: min_num_peaks = 3
+                     threshold_shift_fac = 0.3, threshold_corr = 0.7, min_win_len_fac = 1.5, &
+                     max_noise_window = 5.0, max_energy_ratio = 5.0
+    integer :: min_peaks_troughs = 3
   end type win_config
 
   type :: win_sel_type
@@ -80,11 +83,14 @@ contains
 
   subroutine gen_good_windows(this)
     class(win_sel_type), intent(inout) :: this
-    integer :: i, n_jump, n_groups, group_start, group_end, ib, ie
+    integer :: i, n_jump, n_groups, group_start, group_end, ib, ie, iwin, n_peaks, &
+               groups(max_good_win, 2)
+    integer, allocatable, dimension(:) :: max_peaks, min_peaks, all_peaks
     logical, allocatable, dimension(:) :: good_windows, good_cc, good_shift, good_time
-    logical :: in_group
     real(kind=dp), allocatable :: time_shift_grad(:)
-    real(kind=dp) :: groups(1000, 2)
+    logical :: in_group, good_groups(max_good_win)
+    real(kind=dp) :: max_amp, eng_dat, eng_syn
+    
 
     good_cc = (this%cc_coe > win_config_global%threshold_corr)
     good_shift = (abs(this%time_shift) < win_config_global%threshold_shift_fac * this%min_period)
@@ -96,7 +102,8 @@ contains
     do concurrent (i = 2:size(this%time_shift))
       time_shift_grad(i) = this%time_shift(i) - this%time_shift(i-1)
     end do
-  
+
+    ! Mark windows around large jumps as bad
     do i = 1, size(this%time_shift)
       if (abs(time_shift_grad(i)) > this%jump_buffer) then
         n_jump = nint(this%jump_buffer / this%dt)
@@ -106,8 +113,10 @@ contains
       end if
     end do
 
+    ! Identify continuous good window segments
     n_groups = 0
     in_group = .false.
+    good_groups = .false.
     group_start = 0
     do i = 1, size(this%time_shift)
       if (good_windows(i) .and. .not. in_group) then
@@ -117,6 +126,7 @@ contains
         in_group = .false.
         group_end = i - 1
         n_groups = n_groups + 1
+        good_groups(n_groups) = .true.
         groups(n_groups, 1) = group_start
         groups(n_groups, 2) = group_end
       end if
@@ -125,17 +135,76 @@ contains
     if (in_group) then
       group_end = size(good_windows)
       n_groups = n_groups + 1
+      good_groups(n_groups) = .true.
       groups(n_groups, 1) = group_start
       groups(n_groups, 2) = group_end
     end if
 
-    this%n_win = n_groups
+    ! Reject windows
+    do i = 1, n_groups
+      ! time length too short
+      if ((groups(i, 2) - groups(i, 1)) * this%dt < &
+           win_config_global%min_win_len_fac * this%min_period) then
+        good_groups(i) = .false.
+        cycle
+      end if
+
+      ! number of peaks too few
+      max_peaks = find_maxima(this%syn(groups(i, 1):groups(i, 2)))
+      min_peaks = find_maxima(-this%syn(groups(i, 1):groups(i, 2)))
+      n_peaks = size(max_peaks) + size(min_peaks)
+      max_peaks = find_maxima(this%dat(groups(i, 1):groups(i, 2)))
+      min_peaks = find_maxima(-this%dat(groups(i, 1):groups(i, 2)))
+      n_peaks = min(n_peaks, size(max_peaks) + size(min_peaks))
+      if (n_peaks < win_config_global%min_peaks_troughs) then
+        good_groups(i) = .false.
+        cycle
+      end if
+
+      ! peaks too close
+      all_peaks = merge_sorted_arrays(min_peaks, max_peaks)
+      do ib = 2, size(all_peaks)
+        if ((all_peaks(ib) - all_peaks(ib-1)) * this%dt < 0.5_dp * this%min_period) then
+          good_groups(i) = .false.
+          exit
+        end if
+      end do
+      if (.not. good_groups(i)) cycle
+
+      ! signal to noise ratio too low
+      max_amp = maxval(abs(this%dat(groups(i, 1):groups(i, 2))))
+      if (max_amp / this%noise_level < win_config_global%max_noise_window) then
+        good_groups(i) = .false.
+        cycle
+      end if
+
+      ! Energy ratio too low
+      eng_dat = sum(this%dat(groups(i, 1):groups(i, 2))**2) / real(groups(i, 2) - groups(i, 1) + 1, kind=dp)
+      eng_syn = sum(this%syn(groups(i, 1):groups(i, 2))**2) / real(groups(i, 2) - groups(i, 1) + 1, kind=dp)
+      if (eng_dat / eng_syn > win_config_global%max_energy_ratio .or. &
+          eng_dat / eng_syn < 1/win_config_global%max_energy_ratio) then
+        good_groups(i) = .false.
+        cycle
+      end if
+
+    end do
+
+    this%n_win = count(good_groups(1:n_groups))
+    if (this%n_win == 0) then
+      print *, "Warning: No good windows found."
+      return
+    end if
     allocate(this%twin(this%n_win, 2))
     allocate(this%win_samp(this%n_win, 2))
-    this%twin(:, :) = groups(1:this%n_win, :) * this%dt
-    this%win_samp(:, :) = groups(1:this%n_win, :)
+    iwin = 0
+    do i = 1, n_groups
+      if (good_groups(i)) then
+        iwin = iwin + 1
+        this%twin(iwin, :) = groups(i, :) * this%dt
+        this%win_samp(iwin, :) = groups(i, :)
+      end if
+    end do
 
-    deallocate(time_shift_grad)
   end subroutine gen_good_windows
 
   subroutine xcorr_shift_coe(d, s, dt, tshift, cc_max_coef)
@@ -169,5 +238,48 @@ contains
     cc_max_coef = sum(d_shift * s_shift) / sqrt(sum(d_shift**2) * sum(s_shift**2))
 
   end subroutine xcorr_shift_coe
+
+  function merge_sorted_arrays(arr1, arr2) result(merged)
+    integer, intent(in) :: arr1(:), arr2(:)
+    integer, allocatable :: merged(:)
+    integer :: n1, n2, n_total
+    integer :: i1, i2, idx
+    
+    n1 = size(arr1)
+    n2 = size(arr2)
+    n_total = n1 + n2
+    
+    allocate(merged(n_total))
+    
+    i1 = 1
+    i2 = 1
+    idx = 1
+    
+    ! 归并两个已排序的数组
+    do while (i1 <= n1 .and. i2 <= n2)
+      if (arr1(i1) <= arr2(i2)) then
+        merged(idx) = arr1(i1)
+        i1 = i1 + 1
+      else
+        merged(idx) = arr2(i2)
+        i2 = i2 + 1
+      end if
+      idx = idx + 1
+    end do
+    
+    ! 添加剩余的元素
+    do while (i1 <= n1)
+      merged(idx) = arr1(i1)
+      i1 = i1 + 1
+      idx = idx + 1
+    end do
+    
+    do while (i2 <= n2)
+      merged(idx) = arr2(i2)
+      i2 = i2 + 1
+      idx = idx + 1
+    end do
+    
+  end function merge_sorted_arrays
 
 end module win_sel
